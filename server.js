d2ce598e9248213342b3157da79143da4e9b0a28
unknown <koltolli@proton.me>
2026-04-28 22:59:16 -0400

    Fixed emailing contact form

--- server.js
+++ server.js
@@ -97,12 +97,13 @@ app.post('/submit-form', async (req, res) => {
         `);
 
         const transporter = nodemailer.createTransport({
-            host: 'smtp.ethereal.email',
-            port: 587,
-            auth: {
-                user: process.env.EMAIL_USER,
-                pass: process.env.EMAIL_PASS
-            }
+        host: 'smtp.ethereal.email',
+        port: 465, 
+        secure: true,
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+          }
         });
 
         transporter.sendMail({
